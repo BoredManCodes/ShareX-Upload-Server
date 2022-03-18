2f6183d88c1d62b746b5e732ee773b48248e1551
Trent Buckley <78893529+BoredManCodes@users.noreply.github.com>
2022-03-18 22:47:37 +1100

    Return gif on 404 error

--- src/server/routes/err404.js
+++ src/server/routes/err404.js
@@ -1,7 +1,10 @@
+const fs = require('fs-extra');
 async function err404(req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 404;
-    res.render('404');
+    res.setHeader('Content-Type', 'image/gif');
+    res.statusCode = 200;
+    const filePath = `${__dirname}/../404.gif`;
+    const file = fs.readFileSync(filePath);
+    res.send(file)
     res.end();
 }
 module.exports = err404;
